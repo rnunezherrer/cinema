0bb1ac5c292b2cd9e45ebe3ad15beb8610b7a33a
Ricardo Nuñez Herrera <ricardo@elabmexico.com>
2022-01-08 12:40:15 -0700

    chore: Create Movies table

---
+++ database/migrations/2022_01_08_000000_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', '50')
+                ->nullable(false);
+            $table->date('publication_date')
+                ->nullable(false);
+            $table->string('file_path')
+                ->nullable(true);
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
